d6b49eafc89c1ada333dd1050ac4104011a08fcd
Tovi Newman <deartovi@gmail.com>
2017-08-23 00:25:24 -0700

    refactors, new features, overlay setup window, bug fixes, improve style

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+function randomize(maxNum) {
+  return Math.floor(Math.random() * (maxNum + 1));
+}
+var ADJECTIVES = ['Triumphant', 'Flowing', 'Floating', 'Precocious', 'Hornery', 'Flighty', 'Bellicose', 'Arcadian', 'Calamitous', 'Corpulent', 'Effulgent', 'Equanimous', 'Execrable', 'Judicious', 'Meretricious', 'Parsimonious', 'Pendulous', 'Propitious', 'Redolent']
+var NOUNS = ['Swan', 'Heroes', 'Dream', 'Fancy', 'Chaos', 'Insanity', 'Thrill', 'Victory', 'Failure', 'Apprehension', 'Anger', 'Joy', 'Delights', 'Turpitude']
+var name = ADJECTIVES[randomize(ADJECTIVES.length - 1)] + ' ' + NOUNS[randomize(NOUNS.length - 1)];
 var painter;
+
 $(function() {
+  $('#name').val(name);
+
   painter = {
     getCanvas: function() {
       return this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    },
+    eraseOrColor: function() {
+      return this.$eraser.hasClass('erase') ? 'white' : this.fillColor;
     },
     saveToUndo: function() {
       this.undos.push(this.getCanvas());
@@ -11,6 +23,7 @@ $(function() {
     drawShape: function() {
       this.redos = [];
       this.saveToUndo();
+      this.ctx.fillStyle = this.eraseOrColor();
       if (this.shape === 'triangle') this.drawTriangle();
       else if (this.shape === 'square') this.drawSquare();
       else if (this.shape === 'circle') this.drawCircle();
@@ -22,18 +35,15 @@ $(function() {
       this.ctx.lineTo(this.x, this.y - this.shapeSize);
       this.ctx.lineTo(this.x - this.shapeSize, this.y + this.shapeSize);
       this.ctx.closePath();
-      this.ctx.fillStyle = this.fillColor;
       this.ctx.fill();
     },
     drawSquare: function() {
-      this.ctx.fillStyle = this.fillColor;
       this.ctx.fillRect(this.x - this.shapeSize, this.y - this.shapeSize, this.shapeSize * 2, this.shapeSize * 2);
     },
     drawCircle: function() {
       this.ctx.beginPath();
       this.ctx.arc(this.x, this.y, this.shapeSize, 0, 2 * Math.PI);
       this.ctx.closePath();
-      this.ctx.fillStyle = this.fillColor;
       this.ctx.fill();
     },
     clearCanvas: function() {
@@ -58,13 +68,21 @@ $(function() {
       $('#shapes').on('click', 'input', this.handleSelectShape.bind(this));
       $('#clear').click(this.clearCanvas.bind(this));
       this.$colorInput.on('change keyup', this.handleColorInput.bind(this));
-      $('input[type=number]').on('keyup click', this.handleShapeSize.bind(this));
+      $('#shape_size').on('keyup click', this.handleShapeSize.bind(this));
       $('canvas').on('mousemove', this.handlePenMove.bind(this));
       $('canvas').on('mousedown', this.handlePenDown.bind(this)); 
       $('canvas').on('mouseup mouseleave', this.handlePenUp.bind(this));
       $('#eraser').click(this.handleEraser.bind(this));
       $(document).on('keydown', this.handleUndoRedo.bind(this));
       $('#download').on('click', this.handleDownload.bind(this));
+      $('#set_canvas').on('submit', this.handleSetup.bind(this));
+    },
+    handleSetup: function(e) {
+      $('#tint').fadeOut();
+      this.canvas.width = $('#width').val();
+      this.canvas.height = $('#height').val();
+      $('#title').val($('#name').val())
+      this.clearCanvas();
     },
     handleSelectShape: function(e) { 
       this.shape = e.target.getAttribute('id');
@@ -91,13 +109,10 @@ $(function() {
     },
     handleEraser: function(e) {
       $(e.target).toggleClass('erase');
-      this.fillColor = this.fillColor === 'white' ? this.$colorInput.val() : 'white';
     },
     handleUndoRedo: function(e) {
-      e.stopPropagation();
-      
-      if (e.metaKey && e.key === 'x') this.redo();
-      else if (e.metaKey && e.key === 'z') this.undo();
+      if (e.key === 'y') this.redo();
+      else if (e.key === 'z') this.undo();
     },
     handleDownload: function(e) {
       e.target.href = this.canvas.toDataURL();
@@ -107,6 +122,7 @@ $(function() {
       this.canvas = document.querySelector('canvas');
       this.ctx = this.canvas.getContext('2d');
       this.$colorInput = $('#color');
+      this.$eraser = $('#eraser');
 
       this.shape = 'square';
       this.fillColor = 'black';
@@ -117,7 +133,6 @@ $(function() {
       this.undos = [];
       this.redos = [];
       this.bindEvents(); 
-      this.clearCanvas();
     },
   }
   painter.init();
